8cab118066bdf7067f215af912ecdfbc8093545c
Tim Ryan <id@timryan.org>
2016-10-01 20:10:32 -0400

    Split versions.

--- travis.sh
+++ travis.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Upload tessel2.json
+aws s3 cp tessel2.json s3://builds.tessel.io/t2/sdk/tessel2.json --acl public-read || true
+
 if [ ! -d t2-sdk ]; then
   curl -LO https://s3.amazonaws.com/builds.tessel.io/t2/OpenWRT+SDK/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2
   tar xf OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2
@@ -14,11 +17,11 @@ if [ ! -d rust ]; then
   git clone https://github.com/rust-lang/rust
 fi
 
-curl -LO https://static.rust-lang.org/dist/rust-1.12.0-x86_64-unknown-linux-gnu.tar.gz
-tar xf rust-1.12.0-x86_64-unknown-linux-gnu.tar.gz
-rust-1.12.0-x86_64-unknown-linux-gnu/install.sh --prefix=$PWD/rust-root
+curl -LO https://static.rust-lang.org/dist/rust-$RUST_VERSION-x86_64-unknown-linux-gnu.tar.gz
+tar xf rust-$RUST_VERSION-x86_64-unknown-linux-gnu.tar.gz
+rust-$RUST_VERSION-x86_64-unknown-linux-gnu/install.sh --prefix=$PWD/rust-root
 
 ./rust-cross-libs.sh --rust-prefix=$PWD/rust-root --rust-git=rust --target-json=tessel2.json
 
-tar -cvzf t2-rustlib-1.12.0.tar.gz -C /home/travis/build/tessel/t2-rustlib/rust-root/lib/rustlib/tessel2/lib .
-aws s3 cp t2-rustlib-1.12.0.tar.gz s3://builds.tessel.io/t2/sdk/t2-rustlib-1.12.0.tar.gz --acl public-read
+tar -cvzf t2-rustlib-$RUST_VERSION.tar.gz -C /home/travis/build/tessel/t2-rustlib/rust-root/lib/rustlib/tessel2/lib .
+aws s3 cp t2-rustlib-$RUST_VERSION.tar.gz s3://builds.tessel.io/t2/sdk/t2-rustlib-$RUST_VERSION.tar.gz --acl public-read
