dbddc0e5c1e56e6c411b6e404d1e4891306c6218
Tim Ryan <id@timryan.org>
2016-10-03 15:34:43 -0400

    Adds Travis badge.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # t2-rustlib
 
 Rust libstd and core libraries for cross-compilation.
+
+These are regenerated on Travis each time `master` is updated:
+
+[![Build Status](https://travis-ci.org/tessel/t2-rustlib.svg?branch=master)](https://travis-ci.org/tessel/t2-rustlib)
--- travis.sh
+++ travis.sh
@@ -5,7 +5,7 @@ set -e
 # Upload tessel2.json
 sha256sum tessel2.json > tessel2.json.sha256
 aws s3 cp tessel2.json s3://builds.tessel.io/t2/sdk/tessel2.json --acl public-read || true
-aws s3 cp tessel2.json.sha256 s3://builds.tessel.io/t2/sdk/tessel2.json --acl public-read || true
+aws s3 cp tessel2.json.sha256 s3://builds.tessel.io/t2/sdk/tessel2.json.sha256 --acl public-read || true
 
 if [ ! -d t2-sdk ]; then
   curl -LO https://s3.amazonaws.com/builds.tessel.io/t2/OpenWRT+SDK/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2
@@ -29,4 +29,4 @@ tar -cvzf t2-rustlib-$RUST_VERSION.tar.gz -C /home/travis/build/tessel/t2-rustli
 sha256sum t2-rustlib-$RUST_VERSION.tar.gz > t2-rustlib-$RUST_VERSION.tar.gz.sha256
 
 aws s3 cp t2-rustlib-$RUST_VERSION.tar.gz s3://builds.tessel.io/t2/sdk/t2-rustlib-$RUST_VERSION.tar.gz --acl public-read
-aws s3 cp t2-rustlib-$RUST_VERSION.tar.gz.sha256 s3://builds.tessel.io/t2/sdk/t2-rustlib-$RUST_VERSION.tar.gz --acl public-read
+aws s3 cp t2-rustlib-$RUST_VERSION.tar.gz.sha256 s3://builds.tessel.io/t2/sdk/t2-rustlib-$RUST_VERSION.tar.gz.sha256 --acl public-read
